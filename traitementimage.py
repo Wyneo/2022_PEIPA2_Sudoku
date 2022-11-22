ab7e468330659b949df55bbe9544f997340fb421
Alan PETIT <E219130K@etu.polytech.univ-nantes.prive>
2022-11-22 11:53:30 +0100

    Ajout d'une fonction de véricitation du contour séléctionné comme contour de la grille et nettoyage de quelques coquilles

--- traitementimage.py
+++ traitementimage.py
@@ -13,8 +13,6 @@ def seuillage(grille):
     grillegrise = cv2.cvtColor(grille, cv2.COLOR_RGB2GRAY)
     th = grillegrise.copy()
     img = cv2.adaptiveThreshold(th, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 71, 17)
-    #plt.imshow(img)
-    #plt.show()
     return img
 
 """
@@ -24,14 +22,11 @@ Sortie : contour (matrice contenant les contours)
 """
 def Contour(grille,img):
     kernel = np.ones((3,3),np.uint8)
-    grillecopy=grille.copy()
-    #grillegrise=cv2.cvtColor(grille, cv2.COLOR_RGB2GRAY)
+    grillecopy = grille.copy()
     _,thresh=cv2.threshold(img, 110, 255, 0)
-    ouverture= cv2.morphologyEx(thresh,cv2.MORPH_OPEN, kernel)
-    contour,_=cv2.findContours(ouverture, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    ouverture = cv2.morphologyEx(thresh,cv2.MORPH_OPEN, kernel)
+    contour,_ = cv2.findContours(ouverture, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(grillecopy,contour,-1,(0,255,0),1)
-    #plt.imshow(grillecopy)
-    #plt.show()
     contour = list(contour)
     return contour
 
@@ -70,10 +65,14 @@ Entrée : contourGrille (le contour selectionné)
 Sortie : booléen
 '''
 def VerifContour(contourGrille):
-    "effectuer verif"
-    if "condition verif":
-        return True
-    else : return False
+    correct = False
+    peri = cv2.arcLength(contourGrille, True)
+    approx = cv2.approxPolyDP(contourGrille, 0.04 * peri, True)
+    if len(approx) == 4:
+        correct = True
+    else:
+        print("Image illisible")
+    return correct
 
 """
 SimplContour(contourGrille): Trouve les coordonnées des points correspondant aux 4 coins de la grille
@@ -81,14 +80,10 @@ Entrée : contourGrille (grand contour de la grille -> le carré)
 Sortie : simplc (tableau avec les coordonnées des points correspondant aux 4 coins de la grille)
 """
 def SimplContour(contourGrille):
-    #if VerifContour :
-    peri=cv2.arcLength(contourGrille,True)
-    simplc=cv2.approxPolyDP(contourGrille,0.1*peri,True)
+    peri = cv2.arcLength(contourGrille,True)
+    simplc = cv2.approxPolyDP(contourGrille,0.1*peri,True)
     cv2.drawContours(grillecopy2,simplc,-1,(0,255,0),1)
-    #plt.imshow(grillecopy2)
-    #plt.show()
     return simplc
-    'else : return False'
 
 """
 orderContourPoints(contour): Crée un contour à partir des coordonnées des 4 coins de la grille
@@ -105,46 +100,38 @@ Entrée : contourGrille (grand contour de la grille -> le carré), grille
 Sortie : imgDroite (grilledroite, sans les éléments autour)
 """
 def Redresser(contourGrille,grille):
-    simplc=SimplContour(contourGrille)
-    #if simplc:
-    simplc=orderContourPoints(simplc)
-    M1=np.zeros([900,900])
-    pts1=np.float32(simplc)
-    pts2=np.float32([[900,0],[0,0],[0,900],[900,900]])
-    M2=cv2.getPerspectiveTransform(pts1,pts2)
-    grillecopy3=grille.copy()
-    rows,cols=M1.shape
-    imgDroite=cv2.warpPerspective(grillecopy3,M2,(cols,rows))
+    simplc = SimplContour(contourGrille)
+    simplc = orderContourPoints(simplc)
+    M1 = np.zeros([900,900])
+    pts1 = np.float32(simplc)
+    pts2 = np.float32([[900,0],[0,0],[0,900],[900,900]])
+    M2 = cv2.getPerspectiveTransform(pts1,pts2)
+    rows,cols = M1.shape
+    imgDroite = cv2.warpPerspective(grille,M2,(cols,rows))
     return imgDroite
-    '''else :
-        print("Image peu claire")
-        return False'''
 
-imgDroite=Redresser(contourGrille,grille)
-#if imgDroite:
-plt.imshow(imgDroite)
-plt.show()
+if VerifContour(contourGrille):
+    imgDroite = Redresser(contourGrille,grille)
+    plt.imshow(imgDroite)
+    plt.show()
 
 '''
 Fonction SeparCase : Renvoie un tableau dont chaque élément est une case de la grille
 Entrée : imgDroite (grilledroite, sans les éléments autour)
 Sorties : cases : tableau comprenant chaque case de la grille, coord : tableau de coordonnées de chaque case
 '''
+
 def SeparCase(imgDroite):
     grillecopy4 = imgDroite.copy()
-    k=0
     cases = []
     coord = []
     for i in range(0,899,100):
         for j in range(0,899,100):
             cases.append(grillecopy4[i:i+100,j:j+100])
-            #plt.imshow(cases[k])
-            #plt.show()
             coord.append([[i,i+100],[j,j+100]])
-            k+=1
     return cases,coord
 
-#cases = SeparCase(grillecopy4,cases,coord)
+cases = SeparCase(grillecopy4,cases,coord)
 
 '''
 Fonction RedimAndSave : Redimentionne l'image traitée pour utilisation dans le projet et l'enrgistre en tant que fichier jpeg
@@ -152,9 +139,7 @@ Entrée : img (l'image à redmiensionner), x et y (dimensions de l'image finale 
 Sortie : imgfinale (l'image redimensionnée)
 '''
 def RedimAndSave(img,x,y):
-    imgfinale=cv2.resize(img,(x,y), cv2.INTER_CUBIC)
-    #plt.imshow(imgfinale)
-    #plt.show()
+    imgfinale = cv2.resize(img,(x,y), cv2.INTER_CUBIC)
     plt.imsave('ImgFinale.jpeg',imgfinale)
     return imgfinale
 
