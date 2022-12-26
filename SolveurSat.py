ea4034a251137d32e241e2175bc4826608f82058
Alan PETIT <alan.petit@etu.univ-nantes.fr>
2022-12-26 18:22:26 +0000

    Amélioration générale du code

--- SolveurSat.py
+++ SolveurSat.py
@@ -2,86 +2,65 @@ import pysat
 from pysat.formula import CNF
 from pysat.solvers import Solver
 
-
 '''Fonction SolveurSAT : prend en compte une grille de Sudoku non résolue et la résoud à l'aide de SAT
     Entrée : tableau de tableaux représentant une grille de Sudoku non résolue
     Sortie : tableau de la même forme représentant la grille résolue'''
+
 def SolveurSat(G_init):
 
-    #-------------------------------------------------Remplissage du CNF------------------------------------------------
-    CNFA = []
-    for i in range(9):
-        for j in range(9):
-            CNFA.append(((1, (i, j, 1)), (1, (i, j, 2)), (1, (i, j, 3)), (1, (i, j, 4)), (1, (i, j, 5)), (1, (i, j, 6)), (1, (i, j, 7)), (1, (i, j, 8)), (1, (i, j, 9))))
-            for v in range(1, 10):
-                for n in range(1, 10):
-                    if n != v:
-                        CNFA.append(((0, (i, j, v)), (0, (i, j, n))))
-            k = G_init[i][j]
-            CaseI = i // 3 * 3
-            CaseJ = j // 3 * 3
-            if k != 0:
-                CNFA.append(((1, (i, j, k)), (1, (i, j, k))))
-                for m in range(9):
-                    if m != i:
-                        CNFA.append(((0, (i, j, k)), (0, (m, j, k))))
-                for l in range(9):
-                    if l != j:
-                        CNFA.append(((0, (i, j, k)), (0, (i, l, k))))
-                for a in range(0, 3):
-                    for b in range(0, 3):
-                        if (CaseI+a != i) and (CaseJ+b != j):
-                            CNFA.append(((0, (i, j, k)), (0, (CaseI+a, CaseJ+b, k))))
-
-            for v in range(1, 10):
-                for b in range(9):
-                    if b != j:
-                        CNFA.append(((0, (i, j, v)), (0, (i, b, v))))
-                for d in range(9):
-                    if d != i:
-                        CNFA.append(((0, (i, j, v)), (0, (d, j, v))))
-                for a in range(0, 3):
-                    for b in range(0, 3):
-                        if (CaseI+a != i) and (CaseJ+b != j):
-                            CNFA.append(((0, (i, j, v)), (0, (CaseI+a, CaseJ+b, v))))
-
-    #---------------------------------------------Création des dictionnaires--------------------------------------------
+    # ---------------------------------------------Création des dictionnaires--------------------------------------------
     clé = 1
     PvS = {}
     SvP = {}
     for i in range(9):
         for j in range(9):
             for k in range(9):
-                caseSvP = {"Tuple": (i, j, k+1)}
+                caseSvP = {"Tuple": (i, j, k + 1)}
                 SvP[clé] = caseSvP
                 casePvS = {"Clé": clé}
-                PvS[(i, j, k+1)] = casePvS
-                clé = clé+1
+                PvS[(i, j, k + 1)] = casePvS
+                clé = clé + 1
 
-    CNFASolveur = []
+    #-------------------------------------------------Remplissage du CNF------------------------------------------------
+    CNFA = []
+    for i in range(9):
+        for j in range(9):
+            CNFA.append((PvS[(i, j, 1)]["Clé"],PvS[(i, j, 2)]["Clé"],PvS[(i, j, 3)]["Clé"],PvS[(i, j, 4)]["Clé"],PvS[(i, j, 5)]["Clé"],PvS[(i, j, 6)]["Clé"],PvS[(i, j, 7)]["Clé"],PvS[(i, j, 8)]["Clé"],PvS[(i, j, 9)]["Clé"],))
+            for v in range(1, 10):
+                for n in range(1, 10):
+                    if n != v:
+                        CNFA.append((PvS[(i, j, v)]["Clé"]*(-1), PvS[(i, j, n)]["Clé"]*(-1)))
+            k = G_init[i][j]
+            CaseI = i // 3 * 3
+            CaseJ = j // 3 * 3
+            if k != 0:
+                CNFA.append((PvS[(i, j, k)]["Clé"], PvS[(i, j, k)]["Clé"]))
+                for m in range(9):
+                    if m != i:
+                        CNFA.append((PvS[(i, j, k)]["Clé"]*(-1), PvS[(m, j, k)]["Clé"]*(-1)))
+                for l in range(9):
+                    if l != j:
+                        CNFA.append((PvS[(i, j, k)]["Clé"]*(-1), PvS[(i, l, k)]["Clé"]*(-1)))
+                for a in range(0, 3):
+                    for b in range(0, 3):
+                        if (CaseI+a != i) and (CaseJ+b != j):
+                            CNFA.append((PvS[(i, j, k)]["Clé"]*(-1), PvS[(CaseI+a, CaseJ+b, k)]["Clé"]*(-1)))
 
-    #---------------------------------------Conversion des tuples du CNF en entiers-------------------------------------
-
-    for i in range(len(CNFA)):
-        p = []
-        if (CNFA[i][0][0]) == 0:
-            t = -1
-            p.append(((PvS[CNFA[i][0][1]]["Clé"] * t), (PvS[CNFA[i][1][1]]["Clé"] * t)))
-        else:
-            t = 1
-            if len(CNFA[i]) == 2:
-                p.append(((PvS[CNFA[i][0][1]]["Clé"]*t), (PvS[CNFA[i][1][1]]["Clé"]*t)))
-            else :
-                p.append(((PvS[CNFA[i][0][1]]["Clé"] * t), (PvS[CNFA[i][1][1]]["Clé"] * t), (PvS[CNFA[i][2][1]]["Clé"] * t), (PvS[CNFA[i][3][1]]["Clé"] * t), (PvS[CNFA[i][4][1]]["Clé"] * t), (PvS[CNFA[i][5][1]]["Clé"] * t), (PvS[CNFA[i][6][1]]["Clé"] * t), (PvS[CNFA[i][7][1]]["Clé"] * t), (PvS[CNFA[i][8][1]]["Clé"] * t)))
-
-        CNFASolveur.append(p[0])
+            for v in range(1, 10):
+                for b in range(9):
+                    if b != j:
+                        CNFA.append((PvS[(i, j, v)]["Clé"]*(-1), PvS[(i, b, v)]["Clé"]*(-1)))
+                for d in range(9):
+                    if d != i:
+                        CNFA.append((PvS[(i, j, v)]["Clé"]*(-1), PvS[(d, j, v)]["Clé"]*(-1)))
+                for a in range(0, 3):
+                    for b in range(0, 3):
+                        if (CaseI+a != i) and (CaseJ+b != j):
+                            CNFA.append((PvS[(i, j, v)]["Clé"]*(-1), PvS[(CaseI+a, CaseJ+b, v)]["Clé"]*(-1)))
 
     #-------------------------------------------------Résolution par SAT------------------------------------------------
-    #print('nombre de clauses : ', len(CNFASolveur))
-    cnf = CNF(from_clauses=CNFASolveur)
-
+    cnf = CNF(from_clauses=CNFA)
     with Solver(bootstrap_with=cnf) as solver:
-        #print('formula is', f'{"s" if solver.solve() else "uns"}atisfiable')
         if solver.solve():
             sol = solver.get_model()
         else :
@@ -97,4 +76,3 @@ def SolveurSat(G_init):
         G_final[soluce[i][0]][soluce[i][1]] = soluce[i][2]
 
     return (G_final)
-
